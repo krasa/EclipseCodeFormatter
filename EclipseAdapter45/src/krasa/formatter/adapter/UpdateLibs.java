ecdaae0251808f6c40003ae5ef45de01b1d56805
Vojtech Krasa <vojta.krasa@gmail.com>
2016-01-26 01:19:23 +0100

    utility for updating jars

--- EclipseAdapter45/src/krasa/formatter/adapter/UpdateLibs.java
+++ EclipseAdapter45/src/krasa/formatter/adapter/UpdateLibs.java
@@ -8,23 +8,56 @@ import org.jetbrains.annotations.NotNull;
 
 import com.google.common.base.Predicate;
 import com.google.common.io.Files;
+import org.junit.Assert;
+
+import static org.apache.commons.io.FileUtils.readFileToString;
+import static org.apache.commons.io.FileUtils.write;
+
 
 public class UpdateLibs {
-	public static void main(String[] args) {
-		String to = "lib/eclipse45";
-		String from = "F:\\workspace\\eclipse-jee-mars-1-win32X\\eclipse";
 
-		Set<String> jarNames = getJarsToupdate(to);
-		for (String jarName : jarNames) {
+	private static final File CLASSLOADERS = new File("./src/java/krasa/formatter/eclipse/Classloaders.java");
+
+	public static void main(String[] args) throws IOException {
+		Assert.assertTrue(CLASSLOADERS.exists());
+		new UpdateLibs().run();
+	}
+
+	private void run() throws IOException {
+		String to = "lib/eclipse45";
+		String from = "F:/workspace/eclipse-cpp-mars-1-win32";
+
+		Map<String, String> oldJars = getJarsToUpdate(to);
+		for (Map.Entry<String, String> jarName : oldJars.entrySet()) {
 			System.out.println(jarName);
 		}
 
-		List<File> jarsToCopy = getJarsToCopy(from, jarNames);
+		List<File> jarsToCopy = getJarsToCopy(from, oldJars.keySet());
 
 		copyJars(to, jarsToCopy);
+		
+		updateClassloaders(jarsToCopy, oldJars);
+
 	}
 
-	private static void copyJars(String to, List<File> jarsToCopy) {
+	private void updateClassloaders(List<File> jarsToCopy, Map<String, String> oldJars) throws IOException {
+		String s = readFileToString(CLASSLOADERS);
+
+		for (File file : jarsToCopy) {
+			String prefix = jarPrefix(file.getName());
+			String oldJar = oldJars.get(prefix);
+			s = s.replace(oldJar, file.getName());
+		}
+		write(CLASSLOADERS, s);
+	}
+
+	@NotNull
+	private String jarPrefix(String name) {
+		int i = name.indexOf("_");
+		return name.substring(0, i);
+	}
+
+	private void copyJars(String to, List<File> jarsToCopy) {
 		for (File file : jarsToCopy) {
 			File to1 = new File(to, file.getName());
 			try {
@@ -38,7 +71,7 @@ public class UpdateLibs {
 	}
 
 	@NotNull
-	private static List<File> getJarsToCopy(String from, Set<String> jarNames) {
+	private List<File> getJarsToCopy(String from, Set<String> jarNames) {
 		List<File> jarsToCopy = new ArrayList<File>();
 		Iterator<File> eclipseJars = Files.fileTreeTraverser().breadthFirstTraversal(new File(from)).filter(
 				new Predicate<File>() {
@@ -61,8 +94,8 @@ public class UpdateLibs {
 	}
 
 	@NotNull
-	private static Set<String> getJarsToupdate(String to) {
-		Set<String> jarNames = new HashSet<String>();
+	private Map<String, String> getJarsToUpdate(String to) {
+		Map<String, String> oldJars = new HashMap<String, String>();
 		Iterator<File> iterator = Files.fileTreeTraverser().children(new File(to)).iterator();
 		while (iterator.hasNext()) {
 			File next = iterator.next();
@@ -71,10 +104,10 @@ public class UpdateLibs {
 				int i = name.indexOf("_");
 				if (i <= 0)
 					continue;
-				jarNames.add(name.substring(0, i));
+				oldJars.put(name.substring(0, i), name);
 			}
 		}
-		return jarNames;
+		return oldJars;
 	}
 
 }
