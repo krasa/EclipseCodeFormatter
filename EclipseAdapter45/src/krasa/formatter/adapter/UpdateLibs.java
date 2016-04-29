6db770e67c1a7564a3f726bc20d7e1ed84feb30a
Vojtech Krasa <vojta.krasa@gmail.com>
2016-04-29 07:07:33 +0200

    jar tools

--- EclipseAdapter45/src/krasa/formatter/adapter/UpdateLibs.java
+++ EclipseAdapter45/src/krasa/formatter/adapter/UpdateLibs.java
@@ -1,46 +1,42 @@
 package krasa.formatter.adapter;
 
+import static org.apache.commons.io.FileUtils.readFileToString;
+import static org.apache.commons.io.FileUtils.write;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 import org.jetbrains.annotations.NotNull;
+import org.junit.Assert;
 
 import com.google.common.base.Predicate;
 import com.google.common.io.Files;
-import org.junit.Assert;
-
-import static org.apache.commons.io.FileUtils.readFileToString;
-import static org.apache.commons.io.FileUtils.write;
-
 
 public class UpdateLibs {
 
 	private static final File CLASSLOADERS = new File("./src/java/krasa/formatter/eclipse/Classloaders.java");
 
 	public static void main(String[] args) throws IOException {
-		Assert.assertTrue(CLASSLOADERS.exists());
 		new UpdateLibs().run();
 	}
 
 	private void run() throws IOException {
-		String to = "lib/eclipse45";
-		String from = "F:/workspace/eclipse-cpp-mars-1-win32";
+		String from = "F:\\workspace\\eclipse-jee 4.5.2";
+		File currentJars = new File("lib/eclipse45");
+		File copyTo = new File(currentJars + "/temp");
 
-		Map<String, String> oldJars = getJarsToUpdate(to);
-		for (Map.Entry<String, String> jarName : oldJars.entrySet()) {
-			System.out.println(jarName);
-		}
+		Map<String, String> oldJars = getJarsToUpdate(currentJars);
 
-		List<File> jarsToCopy = getJarsToCopy(from, oldJars.keySet());
+		List<File> jarsToCopy = getJarsToCopy(oldJars.keySet(), new File(from));
 
-		copyJars(to, jarsToCopy);
-		
+		copyJars(jarsToCopy, copyTo);
+
 		updateClassloaders(jarsToCopy, oldJars);
-
 	}
 
 	private void updateClassloaders(List<File> jarsToCopy, Map<String, String> oldJars) throws IOException {
+		Assert.assertTrue(CLASSLOADERS.exists());
 		String s = readFileToString(CLASSLOADERS);
 
 		for (File file : jarsToCopy) {
@@ -57,23 +53,31 @@ public class UpdateLibs {
 		return name.substring(0, i);
 	}
 
-	private void copyJars(String to, List<File> jarsToCopy) {
-		for (File file : jarsToCopy) {
-			File to1 = new File(to, file.getName());
-			try {
-				System.out.println("copying " + file.getName());
-				Files.copy(file, to1);
-			} catch (IOException e) {
-				e.printStackTrace();
-				continue;
+	@NotNull
+	private Map<String, String> getJarsToUpdate(File t) {
+		Map<String, String> oldJars = new HashMap<String, String>();
+		Iterator<File> iterator = Files.fileTreeTraverser().children(t).iterator();
+		while (iterator.hasNext()) {
+			File next = iterator.next();
+			String name = next.getName();
+			if (name.endsWith(".jar")) {
+				int i = name.indexOf("_");
+				if (i <= 0)
+					continue;
+				oldJars.put(name.substring(0, i), name);
 			}
 		}
+		System.out.println("Old jars (" + oldJars.size() + ") found in " + t.getAbsolutePath());
+		for (Map.Entry<String, String> jarName : oldJars.entrySet()) {
+			System.out.println("\t" + jarName.getValue());
+		}
+		return oldJars;
 	}
 
 	@NotNull
-	private List<File> getJarsToCopy(String from, Set<String> jarNames) {
+	private List<File> getJarsToCopy(Set<String> jarNames, File root) {
 		List<File> jarsToCopy = new ArrayList<File>();
-		Iterator<File> eclipseJars = Files.fileTreeTraverser().breadthFirstTraversal(new File(from)).filter(
+		Iterator<File> eclipseJars = Files.fileTreeTraverser().breadthFirstTraversal(root).filter(
 				new Predicate<File>() {
 					@Override
 					public boolean apply(File file) {
@@ -90,24 +94,25 @@ public class UpdateLibs {
 				jarsToCopy.add(next);
 			}
 		}
+		System.out.println("New jars (" + jarNames.size() + ") found in " + root.getAbsolutePath());
+		for (File jarName : jarsToCopy) {
+			System.out.println("\t" + jarName.getName());
+		}
 		return jarsToCopy;
 	}
 
-	@NotNull
-	private Map<String, String> getJarsToUpdate(String to) {
-		Map<String, String> oldJars = new HashMap<String, String>();
-		Iterator<File> iterator = Files.fileTreeTraverser().children(new File(to)).iterator();
-		while (iterator.hasNext()) {
-			File next = iterator.next();
-			String name = next.getName();
-			if (name.endsWith(".jar")) {
-				int i = name.indexOf("_");
-				if (i <= 0)
-					continue;
-				oldJars.put(name.substring(0, i), name);
+	private void copyJars(List<File> jarsToCopy, File to) {
+		System.out.println("Copying to " + to.getAbsolutePath());
+		to.mkdirs();
+		for (File file : jarsToCopy) {
+			File to1 = new File(to, file.getName());
+			try {
+				System.out.println("\t" + file.getName());
+				Files.copy(file, to1);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
 			}
 		}
-		return oldJars;
 	}
 
 }
---
+++ test/java/krasa/RepackJars.java
@@ -0,0 +1,218 @@
+package krasa;
+
+import static java.util.jar.Pack200.Packer.*;
+
+import java.io.*;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Pack200;
+
+import org.apache.commons.io.FileUtils;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.io.Files;
+
+@SuppressWarnings("Duplicates")
+public class RepackJars {
+	private static final Logger log = LoggerFactory.getLogger(RepackJars.class);
+	private static final String SOURCE = "lib/eclipse45";
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		new RepackJars().execute();
+	}
+
+	public void execute() throws IOException, InterruptedException {
+		List<File> files = getJars(new File(SOURCE));
+		// List<File> files = Arrays.asList(new
+		// File("F:\\workspace\\_projekty\\Github\\EclipseCodeFormatter4\\lib\\eclipse45\\org.eclipse.jdt.core_3.11.1.v20150902-1521.jar"));
+
+		for (File jar : files) {
+			File destDir = new File(jar.getParent(), "temp");
+			destDir.mkdir();
+			File destJar = new File(destDir, jar.getName());
+
+			removeCrap(jar, destJar);
+			repack(destJar);
+		}
+
+	}
+
+	public void removeCrap(File srcJarFile, File dest) throws IOException {
+		File tmpJarFile = File.createTempFile("tempJar", ".tmp");
+		tmpJarFile.deleteOnExit();
+		JarFile jarFile = new JarFile(srcJarFile);
+		boolean jarUpdated = false;
+
+		try {
+			JarOutputStream tempJarOutputStream = new JarOutputStream(new FileOutputStream(tmpJarFile));
+
+			try {
+				Enumeration jarEntries = jarFile.entries();
+				while (jarEntries.hasMoreElements()) {
+					JarEntry entry = (JarEntry) jarEntries.nextElement();
+					if (!entry.getName().startsWith("org")) {
+						// System.err.println("removing crap: " + entry.getName());
+						continue;
+					}
+					InputStream entryInputStream = jarFile.getInputStream(entry);
+					tempJarOutputStream.putNextEntry(entry);
+					byte[] buffer = new byte[1024];
+					int bytesRead = 0;
+					while ((bytesRead = entryInputStream.read(buffer)) != -1) {
+						tempJarOutputStream.write(buffer, 0, bytesRead);
+					}
+				}
+
+				jarUpdated = true;
+			} catch (Exception ex) {
+				throw new RuntimeException(ex);
+			} finally {
+				tempJarOutputStream.close();
+			}
+
+		} finally {
+			jarFile.close();
+
+			if (!jarUpdated) {
+				tmpJarFile.delete();
+			}
+		}
+
+		if (jarUpdated) {
+			FileUtils.deleteQuietly(dest);
+			tmpJarFile.renameTo(dest);
+			System.out.println(
+					"crap removed: " + dest.getName() + " (" + size(dest) + ", original " + size(srcJarFile) + ")");
+		} else {
+			throw new RuntimeException(srcJarFile.getAbsolutePath() + " not updated.");
+		}
+	}
+
+	@NotNull
+	private List<File> getJars(File dir) {
+		List<File> files = new ArrayList<File>();
+		for (File next : Files.fileTreeTraverser().children(dir)) {
+			String name = next.getName();
+			int i = name.indexOf("_");
+			if (i <= 0)
+				continue;
+			if (name.endsWith(".jar")) {
+				files.add(next);
+			}
+		}
+		return files;
+	}
+
+	@NotNull
+	private void repack(File file) throws IOException {
+		try {
+			Pack200Utils.normalize(file, properties());
+			System.out.println("compressed:   " + file.getName() + " (" + size(file) + ")");
+		} catch (IOException ioe) {
+			throw new RuntimeException(ioe);
+		}
+
+	}
+
+	@NotNull
+	private String size(File file) {
+		double bytes = file.length();
+		long kilobytes = (long) (bytes / 1024);
+		return kilobytes + " kb";
+	}
+
+	@NotNull
+	private Map<String, String> properties() {
+		// Create the Packer object
+		Pack200.Packer packer = Pack200.newPacker();
+		Map<String, String> p = packer.properties();
+		p.put(Pack200.Packer.EFFORT, "9"); // default is "5"
+		p.put(Pack200.Packer.SEGMENT_LIMIT, "-1");
+		p.put(Pack200.Packer.KEEP_FILE_ORDER, Pack200.Packer.FALSE);
+		p.put(Pack200.Packer.MODIFICATION_TIME, Pack200.Packer.LATEST);
+		p.put(Pack200.Packer.DEFLATE_HINT, Pack200.Packer.TRUE); // compression enabled
+		p.put("com.sun.java.util.jar.pack.verbose", Pack200.Packer.FALSE);
+		String[] attributes = { UNKNOWN_ATTRIBUTE, CLASS_ATTRIBUTE_PFX, FIELD_ATTRIBUTE_PFX, METHOD_ATTRIBUTE_PFX,
+				CODE_ATTRIBUTE_PFX, };
+		String[] stripCodeAttributes = { "SourceFile", "LineNumberTable", "LocalVariableTable", "Deprecated" };
+		for (String attribute : attributes) {
+			for (String attributeName : stripCodeAttributes) {
+				p.put(attribute + attributeName, Pack200.Packer.STRIP);
+			}
+		}
+		p.put(Pack200.Packer.UNKNOWN_ATTRIBUTE, Pack200.Packer.STRIP);
+		return p;
+	}
+
+	/*
+	 * Copyright 2001-2016 The Apache Software Foundation.
+	 *
+	 * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+	 * with the License. You may obtain a copy of the License at
+	 *
+	 * http://www.apache.org/licenses/LICENSE-2.0
+	 *
+	 * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+	 * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+	 * the specific language governing permissions and limitations under the License.
+	 */
+	public static class Pack200Utils {
+		private Pack200Utils() {
+		}
+
+		public static void normalize(File jar) throws IOException {
+			normalize(jar, jar, (Map) null);
+		}
+
+		public static void normalize(File jar, Map<String, String> props) throws IOException {
+			normalize(jar, jar, props);
+		}
+
+		public static void normalize(File from, File to) throws IOException {
+			normalize(from, to, (Map) null);
+		}
+
+		public static void normalize(File from, File to, Map<String, String> props) throws IOException {
+			if (props == null) {
+				props = new HashMap();
+			}
+
+			((Map) props).put("pack.segment.limit", "-1");
+			File f = File.createTempFile("commons-compress", "pack200normalize");
+			f.deleteOnExit();
+
+			try {
+				Object os = new FileOutputStream(f);
+				Object j = null;
+
+				try {
+					Pack200.Packer p = Pack200.newPacker();
+					p.properties().putAll((Map) props);
+					p.pack(new JarFile(from), (OutputStream) os);
+					j = null;
+					((OutputStream) os).close();
+					os = null;
+					Pack200.Unpacker u = Pack200.newUnpacker();
+					os = new JarOutputStream(new FileOutputStream(to));
+					u.unpack(f, (JarOutputStream) os);
+				} finally {
+					if (j != null) {
+						((JarFile) j).close();
+					}
+
+					if (os != null) {
+						((OutputStream) os).close();
+					}
+
+				}
+			} finally {
+				f.delete();
+			}
+
+		}
+	}
+}
