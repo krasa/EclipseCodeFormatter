e949397662ba15917455980099e3cf68c4d451b8
Vojtěch Krása <vojta.krasa@gmail.com>
2024-09-24 13:15:59 +0200

    gradle build

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## [Unreleased]
+
+Bundled Eclipse 2024-09
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,63 +1,28 @@
+@file:Suppress("HardCodedStringLiteral")
+
+import org.jetbrains.changelog.Changelog
+
+fun properties(key: String) = providers.gradleProperty(key)
+fun environment(key: String) = providers.environmentVariable(key)
+fun Jar.patchManifest() = manifest { attributes("Version" to project.version) }
+
 plugins {
-    id("java")
-    id("org.jetbrains.intellij") version "1.17.0"
+    id("java") // Java support
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
+    alias(libs.plugins.changelog) // Gradle Changelog Plugin
 }
 
-tasks {
-    compileTestJava {
-        options.encoding = "UTF-8"
-    }
-}
+group = properties("pluginGroup").get()
+version = properties("pluginVersion").get()
 
-group = "EclipseCodeFormatter"
-version = "23.4.223.000.0-Eclipse_2024-09"
-
-tasks {
-    patchPluginXml {
-        sinceBuild.set("223.0")
-        untilBuild.set("")
-        changeNotes.set(
-            buildString {
-                append("- 2024.1 EAP compatibility").append("<br>")
-            }
-        )
-    }
-
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
-
-    buildSearchableOptions {
-        enabled = false
-    }
-}
-
+// Configure project's dependencies
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://cache-redirector.jetbrains.com/intellij-dependencies")
+    }
 }
-
-// Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-intellij {
-//    version.set("2023.1")
-    version.set("LATEST-EAP-SNAPSHOT")
-//    version.set("2022.3")
-    type.set("IC") // Target IDE Platform
-    plugins.set(listOf("java"))
-}
-
 
 dependencies {
 
@@ -85,3 +50,96 @@ dependencies {
 
 }
 
+
+kotlin {
+    jvmToolchain(17)
+}
+
+// Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    pluginName = properties("pluginName")
+    version = properties("platformVersion")
+    type = properties("platformType")
+
+    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+    plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+}
+
+
+// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
+changelog {
+    groups.empty()
+    repositoryUrl = properties("pluginRepositoryUrl")
+}
+
+
+
+tasks {
+    buildSearchableOptions {
+        enabled = false
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    compileTestJava {
+        options.encoding = "UTF-8"
+    }
+
+    wrapper {
+        gradleVersion = properties("gradleVersion").get()
+    }
+
+    patchPluginXml {
+        version = properties("pluginVersion")
+        sinceBuild = properties("pluginSinceBuild")
+        untilBuild = properties("pluginUntilBuild")
+
+//        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+//        pluginDescription = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
+//            val start = "<!-- Plugin description -->"
+//            val end = "<!-- Plugin description end -->"
+//
+//            with (it.lines()) {
+//                if (!containsAll(listOf(start, end))) {
+//                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+//                }
+//                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+//            }
+//        }
+
+        val changelog = project.changelog // local variable for configuration cache compatibility
+        // Get the latest available change notes from the changelog file
+        changeNotes = properties("pluginVersion").map { pluginVersion ->
+            with(changelog) {
+                renderItem(
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
+                    Changelog.OutputType.HTML,
+                )
+            }
+        }
+    }
+
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
+
+    signPlugin {
+        certificateChain = environment("CERTIFICATE_CHAIN")
+        privateKey = environment("PRIVATE_KEY")
+        password = environment("PRIVATE_KEY_PASSWORD")
+    }
+
+    publishPlugin {
+        dependsOn("patchChangelog")
+        token = environment("PUBLISH_TOKEN")
+        // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
+        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+//        channels = properties("pluginVersion").map { listOf(it.split('-').getOrElse(1) { "default" }.split('.').first()) }
+    }
+}
